4a4c16237c9912bf69c4a33a476b96721113bfb0
manthu24 <msaxena.111@gmail.com>
2026-05-09 03:43:39 +0530

    setting deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 // setup real-time socket connection
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] }
+  cors: { origin: true, methods: ["GET", "POST"] }
 });
 
 app.use(cors());
---
+++ frontend/src/utils/Config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:5000" 
+  : "https://skillswap-rtl2.onrender.com";
